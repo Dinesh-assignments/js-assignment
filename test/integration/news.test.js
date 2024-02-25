b343c141ef5c8daa7856db9beb09432c9c9c20bd
Dinesh-assignments <83549064+Dinesh-assignments@users.noreply.github.com>
2024-02-25 13:33:06 +0530

    Update news.test.js

--- test/integration/news.test.js
+++ test/integration/news.test.js
@@ -1,12 +1,7 @@
 const request = require('supertest');
 const { app } = require('../../index');
 
-describe('Integration Tests', () => {
-
-  it('should return a 200 OK status code for GET request to /health', async () => {
-    const response = await request(app).get('/health');
-    expect(response.status).toBe(200);
-  });
+describe('News Tests', () => {
 
   it('Should return a 200 OK status code when adding match news via a POST request to /add/news, with an expected response body', async () => {
     const postData = {
