786adea8f8bff3d854cad6ed06fe86e43044479d
Dinesh-assignments <83549064+Dinesh-assignments@users.noreply.github.com>
2024-02-25 13:31:49 +0530

    Update health.test.js

--- test/integration/health.test.js
+++ test/integration/health.test.js
@@ -1,25 +1,10 @@
 const request = require('supertest');
 const { app } = require('../../index');
 
-describe('Integration Tests', () => {
+describe('Health Tests', () => {
 
-    it('Should return a 200 OK status code when adding tour news via a POST request to /add/news, with an expected response body', async () => {
-        const postData = {
-          title: 'Tour News',
-          description: 'Tour News Description',
-          tourId: 1
-        };
-    
-        const expectedResponseBody = {
-          status: 'Success',
-          message: 'Added News Successfully'
-        };
-    
-        const response = await request(app)
-          .post('/add/news')
-          .send(postData);
-    
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual(expectedResponseBody);
-      });
+  it('should return a 200 OK status code for GET request to /health', async () => {
+    const response = await request(server).get('/health');
+    expect(response.status).toBe(200);
+  });
 });
