78142c231a3ee686ab5673c8b7c60c5168d0cdd4
Dinesh-assignments <83549064+Dinesh-assignments@users.noreply.github.com>
2024-02-25 13:03:00 +0530

    Add files via upload

--- src/exceptions/newsExceptions.js
+++ src/exceptions/newsExceptions.js
@@ -1,1 +1,11 @@
-
+class NewsException extends Error {
+    constructor (message) {
+      super(message)
+      this.name = 'NEWS_ERROR'
+      this.message = message
+    }
+}
+  
+module.exports = {
+    NewsException: NewsException
+}
