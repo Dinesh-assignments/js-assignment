75f78cc2980a896addc4ca970378bbab3b0edfa8
Dinesh-assignments <83549064+Dinesh-assignments@users.noreply.github.com>
2024-02-25 12:53:58 +0530

    Add Config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,34 @@
+const config = {
+  development: {
+    server: {
+      port: 3000,
+      host: 'localhost'
+    },
+    database: {
+      host: 'db',
+      port: 3307,
+      username: 'myuser',
+      password: 'mypassword',
+      database: 'mydb'
+    },
+  },
+  test: {
+    server: {
+      port: 3000,
+      host: 'localhost'
+    },
+    database: {
+      host: 'localhost',
+      port: 3307,
+      username: 'myuser',
+      password: 'mypassword',
+      database: 'mydb'
+    },
+  }
+};
 
+const env = 'development';
+
+const configuration = config[env];
+
+module.exports = configuration;
