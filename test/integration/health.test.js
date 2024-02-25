b10acdf340f8cac5c30a02d75c49272ac636716d
Dinesh-assignments <83549064+Dinesh-assignments@users.noreply.github.com>
2024-02-25 14:31:50 +0530

    Update health.test.js

--- test/integration/health.test.js
+++ test/integration/health.test.js
@@ -4,7 +4,7 @@ const { app } = require('../../index');
 describe('Health Tests', () => {
 
   it('should return a 200 OK status code for GET request to /health', async () => {
-    const response = await request(server).get('/health');
+    const response = await request(app).get('/health');
     expect(response.status).toBe(200);
   });
 });
